21f4c03c1e63f2c74b81f9a0c7e16fd191a2e6dc
charlykop <charly-ko@hotmail.de>
2023-12-29 20:07:43 +0100

    Completed compoment train_random_forest incl. main.py and run.py and added mlflow run for component test_regression_model

--- main.py
+++ main.py
@@ -50,6 +50,8 @@ def go(config: DictConfig):
             )
 
         if "basic_cleaning" in active_steps:
+
+            ##################
             # Clean data and upload clean data to W&B 
              _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "basic_cleaning"),
@@ -63,8 +65,11 @@ def go(config: DictConfig):
                     "max_price": config['etl']['max_price']
                 },
             )
+            ##################
 
         if "data_check" in active_steps:
+            ##################
+            # Test input data is expected data
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "src", "data_check"),
                 "main",
@@ -76,25 +81,26 @@ def go(config: DictConfig):
                     "max_price": config['etl']['max_price']
                 },
             )
+            ##################
 
         if "data_split" in active_steps:
             ##################
-            # Implement here #
+            # Split data into train and test set
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                version='main',
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"]
+                },
+            )
             ##################
-            # _ = mlflow.run(
-            #     f"{config['main']['components_repository']}/train_val_test_split",
-            #     "main",
-            #     version='main',
-            #     parameters={
-            #         "input": #Input Daten ergänzen,
-            #         "test_size": config["modeling"]["test_size"],
-            #         "random_seed": config["modeling"]["random_seed"],
-            #         "stratify_by": config["modeling"]["stratify_by"]
-            #     },
-            # )
-            pass
 
         if "train_random_forest" in active_steps:
+            # Train random forest model
 
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
@@ -105,28 +111,35 @@ def go(config: DictConfig):
             # step
 
             ##################
-            # Implement here #
+            _ = mlflow.run(
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "rf_config": rf_config,
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "output_artifact": "random_forest_export"
+                },
+            )
             ##################
 
-            pass
 
         if "test_regression_model" in active_steps:
 
             ##################
-            # Implement here #
+            _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                version='main',
+                parameters={
+                    "mlflow_model": "random_forest_export:prod",
+                    "test_dataset": "test_data.csv:latest"
+                },
+            )
             ##################
-
-            # _ = mlflow.run(
-            #     f"{config['main']['components_repository']}/test_regression_model",
-            #     "main",
-            #     version='main',
-            #     parameters={
-            #         "mlflow_model": #Modell hier ergänzen,
-            #         "test_dataset": # Testdaten hier ergänzen
-            #     },
-            # )
-
-            pass
 
 
 if __name__ == "__main__":
